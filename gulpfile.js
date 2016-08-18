4c2bf6f82216a6a1b91993a378ab9aad511939d9
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-08-18 20:46:24 +0700

    build

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,29 @@ var gulp = require('gulp');
 var htmlReplace = require('gulp-html-replace');
 
 gulp.task('htmlReplace',function(){
-     gulp.src('src/gl-form-label.html')
+     gulp.src('src/gl-grid-element.html')
     .pipe(htmlReplace({
         'import': {
                 src:null,
                 tpl: 
                 '<link rel="import" href="../polymer/polymer.html">\n'
-                +'<link rel="import" href="../paper-input/paper-textarea.html">'
+                +'<script src="../eqcss/EQCSS.min.js"></script>'
+        }
+    }))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('htmlReplace2',function(){
+     gulp.src('src/gl-grid.html')
+    .pipe(htmlReplace({
+        'import': {
+                src:null,
+                tpl: 
+                '<link rel="import" href="../polymer/polymer.html">'
         }
     }))
     .pipe(gulp.dest(''));
 });
 
 
-gulp.task('default', ['htmlReplace']);
+gulp.task('default', ['htmlReplace','htmlReplace2']);
